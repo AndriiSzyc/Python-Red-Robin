be2915ca7de6ebc7f0230057b300bcbef42a83f5
AndriiSzyc <75746322+AndriiSzyc@users.noreply.github.com>
2022-11-04 15:04:50 +0200

    add task_3

--- HW#9_Context_manager._Work_with_files/file.py
+++ HW#9_Context_manager._Work_with_files/file.py
@@ -1,5 +1,6 @@
 import datetime
 import csv
+import json
 
 #TASK 1
 class WriteLogs:
@@ -31,7 +32,7 @@ def write_scv(file):
             line_sum = line[0] + ' ' + line[1]
             line = line[2:]
             line.insert(0, line_sum)
-            print(line)
+            #print(line)
             with open('logs.csv', 'a') as csv_file:
                 writer = csv.writer(csv_file, delimiter=',')
                 writer.writerow(line)
@@ -39,3 +40,23 @@ def write_scv(file):
 scv_model = write_scv('logs.txt')
 
 #TASK 3
+
+def count_open(file):
+    count = 0
+    last_open = ''
+    with open(file, newline='') as scv_file:
+        reader = csv.reader(scv_file, delimiter=',')
+        for line in reader:
+            line = ', '.join(line)
+            if 'OPEN' in line:
+                count += 1
+                last_open = line
+
+    line_date = last_open[:last_open.find(',')]
+    name_file = last_open.split(', ')
+    name_file = name_file[1]
+    convert_dict = {name_file: {'count': count, 'last_time_opened': line_date}}
+    with open('logs.json', 'w') as json_file:
+        json.dump(convert_dict, json_file, indent=4)
+
+json_model = count_open('logs.csv')
