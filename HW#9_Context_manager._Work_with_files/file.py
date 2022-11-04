dea0b54484be7a7f8e5454128c93f19b16492739
AndriiSzyc <75746322+AndriiSzyc@users.noreply.github.com>
2022-11-04 12:17:02 +0200

    add file.py, task_1_add_classmethod

--- HW#9_Context_manager._Work_with_files/file.py
+++ HW#9_Context_manager._Work_with_files/file.py
@@ -1,17 +1,24 @@
 import datetime
 
+class WriteLogs:
+    count = 0
+
+    @classmethod
+    def write(cls, time, file):
+        cls.time = time
+        cls.file = file
+        with open('logs.txt', 'a') as logs:
+            write_open = str(cls.time) + ' ' + cls.file + ' OPEN\n' if WriteLogs.count % 2 == 0 else str(cls.time) + ' ' + cls.file + ' CLOSE\n'
+            logs.write(write_open)
+            WriteLogs.count += 1
+
+
 time_now = datetime.datetime.now()
 read_file = 'file.txt'
 with open(read_file, 'r') as file:
-    #print('open')
-    with open('logs.txt', 'a') as logs:
-        write_open = str(time_now) + ' ' + read_file + ' OPEN\n'
-        logs.write(write_open)
+    WriteLogs.write(time_now, read_file)
     file.read()
-    #print('close')
-    with open('logs.txt', 'a') as logs:
-        write_close = str(time_now) + ' ' + read_file + ' CLOSE\n'
-        logs.write(write_close)
+    WriteLogs.write(time_now, read_file)
 
 
 
