3d284bc7fa0add901114a0203ccb9a02a0629732
Andrii Szyc <andrewszyc@ukr.net>
2022-12-03 22:31:48 +0200

    add unittest

--- test_python/main.py
+++ test_python/main.py
@@ -1,12 +1,4 @@
-def numbers(a: int) -> str:
-    string_numbers = {0: 'zero',
-                      1: 'one',
-                      2: 'two',
-                      3: 'three',
-                      4: 'four',
-                      5: 'five'}
-    return string_numbers.get(a)
-
-
-# num = numbers(4)
-# print(num)
+def numbers(a: int) -> str:  # sintax - what return function return (string)
+    return {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four"}.get(
+        a, "Unexpected argument"
+    )
