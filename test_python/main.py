ffb5d5a04256d7143f087382217f0226468948ba
Andrii Szyc <andrewszyc@ukr.net>
2022-12-11 16:24:51 +0200

    add class Taras

--- test_python/main.py
+++ test_python/main.py
@@ -2,3 +2,11 @@ def numbers(a: int) -> str:  # sintax - what return function return (string)
     return {0: "zero", 1: "one", 2: "two", 3: "three", 4: "four"}.get(
         a, "Unexpected argument"
     )
+
+class Taras:
+    def __init__(self, age, sex):
+        self.age = age
+        self.sex = sex
+
+    def question(self):
+        return 'command execution Make show'
--- test_python/tests_pytest.py
+++ test_python/tests_pytest.py
@@ -1,6 +1,18 @@
 import pytest
-from main import numbers
+from main import numbers, Taras
 
+@pytest.fixture
+def taras():
+    return(Taras(age=18, sex='male'))
+
+def test_taras_age(taras):
+    assert taras.age == 18
+
+def test_taras_sex(taras):
+    assert taras.sex == 'male'
+
+def test_taras_age_sex(taras):
+    assert taras.sex != 'female'
 
 def test_number_zero():
     assert numbers(0) == 'zero'
