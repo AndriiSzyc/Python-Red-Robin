59231e90220f725419d9a9f6f06460f2a5eb1ae7
Andrii Szyc <andrewszyc@ukr.net>
2022-12-12 21:58:49 +0200

    add all code for practice

--- Practice_Lecture#12/main.py
+++ Practice_Lecture#12/main.py
@@ -1,10 +1,11 @@
 import sys
+import json
 from argparse import ArgumentParser
 from signers import Signer
 
 
 def args_parser() -> ArgumentParser:
-    parser = ArgumentParser(description='Very strong decoder')
+    parser = ArgumentParser(description="Very strong decoder.")
     parser.add_argument(
         "--secret", type=str, required=True, help="Secret key for encode / decode."
     )
@@ -31,4 +32,33 @@ if __name__ == "__main__":
     parser = args_parser()
     namespace = parser.parse_args(sys.argv[1:])
     sign = Signer(secret=namespace.secret, salt=namespace.salt)
-    print(sign)
+    if namespace.action == "encode":
+        encode: list = []
+        with open(file=namespace.input_file) as file:
+            try:
+                data = json.load(file)
+            except json.decoder.JSONDecodeError:
+                raise "invalid input data..."
+        for count, line in enumerate(data):
+            if namespace.using == "pyjwt":
+                encode.append(sign.jwt_encode(payload=data[count]))
+            if namespace.using == "itsdangerous":
+                encode.append(sign.itsdangerous_encode(payload=data[count]))
+        with open(file=namespace.output_file, mode="w") as file:
+            file.write(json.dumps(encode))
+
+    if namespace.action == "decode":
+        decode: list = []
+        with open(file=namespace.input_file) as file:
+            try:
+                data = json.load(file)
+            except json.decoder.JSONDecodeError:
+                raise "invalid input data..."
+        for count, line in enumerate(data):
+            if namespace.using == "pyjwt":
+                decode.append(sign.jwt_decode(encoded=line))
+            if namespace.using == "itsdangerous":
+                decode.append(sign.itsdangerous_decode(encoded=line))
+
+        with open(file=namespace.output_file, mode="w") as file:
+            file.write(json.dumps(decode))
