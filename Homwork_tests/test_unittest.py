7cd07fd12bd93094914a23bca5da4588475c7973
Andrii Szyc <andrewszyc@ukr.net>
2022-12-13 22:07:08 +0200

    add tests

--- Homwork_tests/test_unittest.py
+++ Homwork_tests/test_unittest.py
@@ -1,52 +1,53 @@
 from unittest import TestCase
 from signers import Signer
 
+
 class Test_Singers(TestCase):
     """Asserts types
-        self.assertEqual(a, b) -> a==b
-        self.assertNotEqual(a, b) -> a!=b
-        self.assertTrue(a) -> bool(a) is True
-        self.assertFalse(a) -> bool(a) is False
-        self.assertIs(a, b) -> a is b
-        self.assertIsNot(a, b) -> a is not b
-        self.assertIsNone(a) -> a is None
-        self.assertIn(a, b) -> a in b
-        self.assertNotIn(a, b) -> a  not in b
-        self.assertIsInstance(a, b) -> isinstance(a, b)
-        """
-    def _create_signer( secret: str, salt: str):
+    self.assertEqual(a, b) -> a==b
+    self.assertNotEqual(a, b) -> a!=b
+    self.assertTrue(a) -> bool(a) is True
+    self.assertFalse(a) -> bool(a) is False
+    self.assertIs(a, b) -> a is b
+    self.assertIsNot(a, b) -> a is not b
+    self.assertIsNone(a) -> a is None
+    self.assertIn(a, b) -> a in b
+    self.assertNotIn(a, b) -> a  not in b
+    self.assertIsInstance(a, b) -> isinstance(a, b)
+    """
+
+    def _create_signer(secret: str, salt: str):
         return Signer()
 
     def setUp(self) -> None:
-        self.sign = Signer(secret='sec', salt='slt')
+        self.sign = Signer(secret="sec", salt="slt")
 
     def test_chek_isinstance_input(self):
-        payload = {'name': 'Ozzy'}
+        payload = {"name": "Ozzy"}
         self.assertIsInstance(payload, dict)
+
     def test_chek_work_correct_input(self):
-        payload = {'name': 'Ozzy'}
+        payload = {"name": "Ozzy"}
         self.assertTrue(self.sign.jwt_encode(payload))
 
-
     def test_jwt_encode(self):
-        payload = {'name': 'Ozzy'}
+        payload = {"name": "Ozzy"}
         encode = self.sign.jwt_encode(payload)
         self.assertIsNot(payload, encode)
 
     def test_jwt_decode(self):
-        payload = {'name': 'Ozzy'}
+        payload = {"name": "Ozzy"}
         encode = self.sign.jwt_encode(payload)
         decode = self.sign.jwt_decode(encode)
         self.assertEqual(decode, payload)
 
     def test_itsdangerous_encode(self):
-        payload = {'name': 'Ozzy'}
+        payload = {"name": "Ozzy"}
         encode = self.sign.itsdangerous_encode(payload)
         self.assertIsNot(payload, encode)
 
     def test_itsdangerous_decode(self):
-        payload = {'name': 'Ozzy'}
+        payload = {"name": "Ozzy"}
         encode = self.sign.itsdangerous_encode(payload)
         decode = self.sign.itsdangerous_decode(encode)
         self.assertEqual(decode, payload)
-
--- Homwork_tests/tests_pytest.py
+++ Homwork_tests/tests_pytest.py
@@ -1,36 +1,38 @@
 import pytest
 from signers import Signer
 
+
 @pytest.fixture
 def sign():
-    return Signer(secret='sec', salt='slt')
+    return Signer(secret="sec", salt="slt")
+
 
 def test_chek_isinstance_input(sign):
-    payload = {'name': 'Ozzy'}
+    payload = {"name": "Ozzy"}
     assert isinstance(payload, dict)
 
 
 def test_jwt_encode(sign):
-    payload = {'name': 'Ozzy'}
+    payload = {"name": "Ozzy"}
     encode = sign.jwt_encode(payload)
     assert payload != encode
 
 
 def test_jwt_decode(sign):
-    payload = {'name': 'Ozzy'}
+    payload = {"name": "Ozzy"}
     encode = sign.jwt_encode(payload)
     decode = sign.jwt_decode(encode)
     assert decode == payload
 
 
 def test_itsdangerous_encode(sign):
-    payload = {'name': 'Ozzy'}
+    payload = {"name": "Ozzy"}
     encode = sign.itsdangerous_encode(payload)
     assert payload != encode
 
 
 def test_itsdangerous_decode(sign):
-    payload = {'name': 'Ozzy'}
+    payload = {"name": "Ozzy"}
     encode = sign.itsdangerous_encode(payload)
     decode = sign.itsdangerous_decode(encode)
     assert decode == payload
