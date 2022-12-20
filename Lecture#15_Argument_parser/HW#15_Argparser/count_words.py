3c5e0cb1f17d59adf4e7e6351fac08c3066a7c8d
Andrii Szyc <andrewszyc@ukr.net>
2022-12-20 15:55:07 +0200

    add task#1

---
+++ Lecture#15_Argument_parser/HW#15_Argparser/count_words.py
@@ -0,0 +1,18 @@
+import argparse
+
+def count_words(file_name):
+    with open(file_name, 'r') as file:
+        words = file.read()
+    count = len(words.split())
+    print(count)
+
+def main():
+    parser = argparse.ArgumentParser('Counts numbers of words in file')
+    parser.add_argument('-f',  action='store_true', help='Count words')
+    parser.add_argument('infile', type=str, help='Input file')
+    args = parser.parse_args()
+    if args.f:
+        count_words(args.infile)
+
+if __name__ == '__main__':
+     main()
