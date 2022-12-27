bc2c64f078c14d34d51c64e38047295ff9b19644
Andrii Szyc <andrewszyc@ukr.net>
2022-12-27 15:54:55 +0200

    add thread_pokemon.py

--- Lecture#16_Threading_ana_Multiprocessing/thread_pokemon.py
+++ Lecture#16_Threading_ana_Multiprocessing/thread_pokemon.py
@@ -5,16 +5,8 @@ import requests
 import asyncio
 from requests.exceptions import HTTPError
 
-# import aiohttp
 
 BASE_URL = "https://pokeapi.co/api/v2/"
-# resp = requests.get(f"{BASE_URL}/pokemon/1")
-# print(resp)
-# print(resp.json())
-# jsonResp = resp.json()
-# for key, value in jsonResp.items():
-#    print(key, ':', value)
-# print(jsonResp['name'], jsonResp['id'])
 
 
 def get_pokemon(number, lock, base_url=BASE_URL):
@@ -22,9 +14,7 @@ def get_pokemon(number, lock, base_url=BASE_URL):
         with lock:
             resp = requests.get(f"{BASE_URL}/pokemon/{number}")
             resp.raise_for_status()
-            print(
-                f" - Id {resp.json()['id']}. Name: {resp.json()['name']}."
-            )
+            print(f" - Id {resp.json()['id']}. Name: {resp.json()['name']}.")
 
     except HTTPError as http_err:
         print(f"HTTP error occured: {http_err}")
