004d78fcffc033328796b8260c8a82840a9cb416
Andrii Szyc <andrewszyc@ukr.net>
2022-12-27 15:43:01 +0200

    made thread pokemon_API.py

--- Lecture#16_Threading_ana_Multiprocessing/pokemon_API.py
+++ Lecture#16_Threading_ana_Multiprocessing/pokemon_API.py
@@ -3,22 +3,39 @@ import threading
 from threading import Thread
 import requests
 import asyncio
-#import aiohttp
+from requests.exceptions import HTTPError
+
+# import aiohttp
 
 BASE_URL = "https://pokeapi.co/api/v2/"
-resp = requests.get(f"{BASE_URL}/pokemon")
-print(resp)
-print(resp.json())
+# resp = requests.get(f"{BASE_URL}/pokemon/1")
+# print(resp)
+# print(resp.json())
+# jsonResp = resp.json()
+# for key, value in jsonResp.items():
+#    print(key, ':', value)
+# print(jsonResp['name'], jsonResp['id'])
 
-def get_pokemon(number, base_url=BASE_URL):
+
+def get_pokemon(number, lock, base_url=BASE_URL):
     try:
-        resp = requests.get(f"{BASE_URL}/pokemon/{number}")
-        print(f"Name: {resp.json()['name']}")
-    except:
-        print('Shit happens')
+        with lock:
+            resp = requests.get(f"{BASE_URL}/pokemon/{number}")
+            resp.raise_for_status()
+            print(
+                f" - Id {resp.json()['id']}. Name: {resp.json()['name']}. Type: {resp.json()['types']}"
+            )
 
-threads = [Thread(target=get_pokemon, args=(number,)) for number in range(20)]
+    except HTTPError as http_err:
+        print(f"HTTP error occured: {http_err}")
+    except Exceptions as err:
+        print(f"Other error occured: {err}")
+
+
+lock = threading.Lock()
+
+threads = [Thread(target=get_pokemon, args=(number, lock)) for number in range(1, 21)]
 start = time.time()
-[t.start() for t in threads];
-[t.join() for t in threads];
+[t.start() for t in threads]
+[t.join() for t in threads]
 print(time.time() - start)
